e31dd251069b4152e06089298ef74a982f3cc89c
bolshiyanov <bolshiyanov@gmail.com>
2021-03-01 20:08:59 +0000

    pwa test v3.2.13 test cache

--- frontend_1/sw.js
+++ frontend_1/sw.js
@@ -1,43 +1,89 @@
-const CACHE = "pwabuilder-page";
+const staticCacheName = 'site-static-v4';
+const dynamicCacheName = 'site-dynamic-v4';
+const assets = [
+  '/',
+  'index.html',
+  'app.js',
+  'plugins/core.js',
+  'plugins/siriwave.js',
+  'plugins/player1.js',
+  'plugins/player2.js',
+  'plugins/player3.js',
+  'plugins/player4.js',
+  'linkCopy.js',
+  'css/index.css',
+  'css/styles.css',
+  'player1.html',
+  'player2.html',
+  'player3.html',
+  'player4.html',
+  'fallback.html',
+  'logo50.png',
+  'images/abundance.jpg',
+  'images/health.jpg',
+  'images/relationships.jpg',
+  'images/sleep.jpg',
+  'images/medisilogo.png',
+  'favicon.png',
+  'meta/favicon-16x16.png',
+  'meta/favicon-32x32.png',
+  'meta/favicon-96x96.png'
+];
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "index.html";
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if(keys.length > size){
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+// install event
+self.addEventListener('install', evt => {
+  console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
   );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
+// activate event
+self.addEventListener('activate', evt => {
+  console.log('service worker activated');
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      console.log(keys);
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
+// fetch events
+self.addEventListener('fetch', evt => {
+  if(evt.request.url.indexOf('admin@cluster0.milmt.mongodb.net') === -1){
+    evt.respondWith(
+      caches.match(evt.request).then(cacheRes => {
+        return cacheRes || fetch(evt.request).then(fetchRes => {
+          return caches.open(dynamicCacheName).then(cache => {
+            cache.put(evt.request.url, fetchRes.clone());
+            // check cached items size
+            limitCacheSize(dynamicCacheName, 2);
+            return fetchRes;
+          })
+        });
+      }).catch(() => {
+        if(evt.request.url.indexOf('.html') > -1){
+          return caches.match('fallback.html');
+        } 
+      })
+    );
   }
 });
